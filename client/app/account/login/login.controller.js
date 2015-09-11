73446367578f96c8357ef580d62d3e583415be40
Mohit Kumar <mhtkmr54@gmail.com>
2015-09-12 01:05:27 +0530

    basic scaffolds

---
+++ client/app/account/login/login.controller.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('temproryRegistrationsApp')
+  .controller('LoginCtrl', function ($scope, Auth, $location, $window) {
+    $scope.user = {};
+    $scope.errors = {};
+
+    $scope.login = function(form) {
+      $scope.submitted = true;
+
+      if(form.$valid) {
+        Auth.login({
+          email: $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          // Logged in, redirect to home
+          console.log("Current user role: " + Auth.getCurrentUser().role);
+          //
+          Auth.isLoggedInAsync(function(success) {
+        console.log("Current user role: " + Auth.getCurrentUser().role);
+        });
+          $location.path('/');
+        })
+        .catch( function(err) {
+          $scope.errors.other = err.message;
+        });
+      }
+    };
+
+    $scope.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
