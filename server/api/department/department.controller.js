d71746a6bd21e685db19f2b66335ad09b0200c41
raakeshkamal <raakeshkamal@gmail.com>
2015-09-13 05:58:00 +0530

    basic backend done

--- server/api/department/department.controller.js
+++ server/api/department/department.controller.js
@@ -5,26 +5,28 @@ var Department = require('./department.model');
 
 // Get list of departments
 exports.index = function(req, res) {
-  Department.find(function (err, departments) {
+  Department.find(function (err, departments){
     if(err) { return handleError(res, err); }
-    return res.status(200).json(departments);
-  });
+    return res.json(200, departments);
+  })
+  .populate('cores coords superCoords qms subDepartments');
 };
 
 // Get a single department
 exports.show = function(req, res) {
   Department.findById(req.params.id, function (err, department) {
     if(err) { return handleError(res, err); }
-    if(!department) { return res.status(404).send('Not Found'); }
+    if(!department) { return res.send(404); }
     return res.json(department);
-  });
+  })
+  .populate('cores coords superCoords qms subDepartments');
 };
 
 // Creates a new department in the DB.
 exports.create = function(req, res) {
   Department.create(req.body, function(err, department) {
     if(err) { return handleError(res, err); }
-    return res.status(201).json(department);
+    return res.json(201, department);
   });
 };
 
@@ -33,11 +35,11 @@ exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
   Department.findById(req.params.id, function (err, department) {
     if (err) { return handleError(res, err); }
-    if(!department) { return res.status(404).send('Not Found'); }
+    if(!department) { return res.send(404); }
     var updated = _.merge(department, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
-      return res.status(200).json(department);
+      return res.json(200, department);
     });
   });
 };
@@ -46,14 +48,14 @@ exports.update = function(req, res) {
 exports.destroy = function(req, res) {
   Department.findById(req.params.id, function (err, department) {
     if(err) { return handleError(res, err); }
-    if(!department) { return res.status(404).send('Not Found'); }
+    if(!department) { return res.send(404); }
     department.remove(function(err) {
       if(err) { return handleError(res, err); }
-      return res.status(204).send('No Content');
+      return res.send(204);
     });
   });
 };
 
 function handleError(res, err) {
-  return res.status(500).send(err);
+  return res.send(500, err);
 }
--- server/api/department/department.model.js
+++ server/api/department/department.model.js
@@ -6,7 +6,23 @@ var mongoose = require('mongoose'),
 var DepartmentSchema = new Schema({
   name: String,
   info: String,
-  active: Boolean
+  calendar: String,
+  folder: String,
+
+  subDepartments: [{ type: Schema.Types.ObjectId, ref: 'SubDepartment' }],
+  cores: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  superCoords: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  coords: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  qms: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  canPost: {},
+  createdOn: {
+  	type: Date,
+  	default: Date.now
+  },
+  updatedOn: {
+  	type: Date,
+  	default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Department', DepartmentSchema);
